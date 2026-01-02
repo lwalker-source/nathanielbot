521728815b1d62b679d4abb4ef1e977fee7575f2
Lawrence Walker <ljwalker196@gmail.com>
2026-01-02 12:22:39 -0800

    Fixed SSL errors.
    Used claude to fix ssl connectivity errors.

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,31 @@
 import discord
 import os
+import certifi
+import ssl
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+
+# Fix SSL certificate issues on macOS
+# Set SSL certificate file path
+os.environ['SSL_CERT_FILE'] = certifi.where()
+
+# Create custom SSL context
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+# Patch aiohttp's TCPConnector to use our SSL context
+import aiohttp.connector
+
+_original_init = aiohttp.connector.TCPConnector.__init__
+
+def _patched_init(self, *args, ssl=None, **kwargs):
+    """Override to use certifi SSL context by default"""
+    if ssl is None:
+        ssl = ssl_context
+    _original_init(self, *args, ssl=ssl, **kwargs)
+
+aiohttp.connector.TCPConnector.__init__ = _patched_init
 
 class MyClient(discord.Client):
   async def on_ready(self):
